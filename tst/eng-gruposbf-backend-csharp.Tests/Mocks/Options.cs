a5def2e9a6d29362e4f53b5563e54105849f7ced
Murillo Nogueira <murillo.nogueira@qdois.com.br>
2022-04-21 10:52:29 -0300

    Testes unitarios healthcheck

---
+++ tst/eng-gruposbf-backend-csharp.Tests/Mocks/Options.cs
@@ -0,0 +1,38 @@
+﻿using eng_gruposbf_backend_csharp.Shared.Settings;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Collections.Generic;
+
+namespace eng_gruposbf_backend_csharp.Mocks
+{
+    public static class Options
+    {
+        public static IOptions<ApiGateway> ReportsApiGateway()
+        {
+            var mock = new Mock<IOptions<ApiGateway>>();
+
+            mock.SetupGet(m => m.Value)
+                .Returns(new ApiGateway
+                {
+                    Resources = new List<Resource>()
+                    {
+                        new Resource
+                        {
+                            Name = "Test Service",
+                            BaseUrl = "http://servico.test",
+                            Parameters = new List<Parameter>()
+                            {
+                                new Parameter
+                                {
+                                    Key = "Chave",
+                                    Value = "Valor"
+                                }
+                            }
+                        }
+                    }
+                });
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tst/eng-gruposbf-backend-csharp.Tests/UseCases/HealthCheckUseCaseUnitTest.cs
@@ -0,0 +1,57 @@
+﻿using eng_gruposbf_backend_csharp.Borders.Repositories.HealthCheck;
+using eng_gruposbf_backend_csharp.UseCases.HealthCheck;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eng_gruposbf_backend_csharp.Tests.UseCases
+{
+    public class HealthCheckUseCaseUnitTest
+    {
+        private readonly Mock<IHealthCheckRepository> _healthCheckRepository;
+        public HealthCheckUseCaseUnitTest()
+        {
+            _healthCheckRepository = new Mock<IHealthCheckRepository>();
+        }
+
+        [Fact]
+        public async Task HealthCheck_UseCase_Ok()
+        {
+            _healthCheckRepository.Setup(h => h.Ping(It.IsAny<string>()))
+                .ReturnsAsync(() => IPStatus.Success.ToString());
+
+            var useCase = new HealthCheckUseCase(
+                _healthCheckRepository.Object,
+                Mocks.Options.ReportsApiGateway());
+
+            var result = await useCase.Execute();
+
+            result.Content.Should().NotBeNull();
+            result.Content.Activities.Should().NotBeNull().And.NotBeEmpty();
+            result.Content.Activities.FirstOrDefault().Should().NotBeNull();
+            result.Content.Activities.FirstOrDefault()?.Message.Should().BeEquivalentTo(IPStatus.Success.ToString());
+        }
+
+        [Fact]
+        public async Task HealthCheck_UseCase_ReturnsExceptions()
+        {
+            _healthCheckRepository.Setup(h => h.Ping(It.IsAny<string>()))
+                .Throws(() => new Exception());
+
+            var useCase = new HealthCheckUseCase(
+                _healthCheckRepository.Object,
+                Mocks.Options.ReportsApiGateway());
+
+            var result = await useCase.Execute();
+
+            result.Errors.Should().NotBeNull().And.NotBeEmpty();
+            result.Errors.FirstOrDefault().Should().NotBeNull();
+        }
+    }
+}
