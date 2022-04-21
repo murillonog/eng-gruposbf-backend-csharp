ca4db801b5cbd1a7e10413ea14eaa203f5adb594
Murillo Nogueira <murillo.nogueira@qdois.com.br>
2022-04-21 08:47:44 -0300

    Estrutura inicial do projeto

---
+++ src/eng-gruposbf-backend-csharp.Api/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace eng_gruposbf_backend_csharp.Api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
